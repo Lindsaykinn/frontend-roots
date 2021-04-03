49ad75046b600d74ee7ae9ec216be331fce1123a
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-03 16:56:22 -0400

    added initialState to familiesReducer

--- src/reducers/familiesReducer.js
+++ src/reducers/familiesReducer.js
@@ -3,7 +3,7 @@ const initialState = {
   loading: true
 }
 
-const familiesReducer = (state, action) => {
+const familiesReducer = (state=initialState, action) => {
   switch(action.type){
     default:
       return state;
