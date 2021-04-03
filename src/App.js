d5bd16757e4d7c809c668251520d5f9eae55ead1
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-03 16:55:49 -0400

    added & imported routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import Navbar from './Navbar'
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Home'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import ErrorPage from './components/Error'
+import List from './components/List'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <Router>
-    <div className="App">
-        <Navbar />
-      <div className="content">
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>      
+          <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path='/'><Home /></Route>
+            <Route component={ ErrorPage }></Route>
+            <Route exact path='/list' component={ List }>All Families</Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loading: state.loading
+  }
+}
+export default connect(mapStateToProps, { getFamilies })(App);
