f826fe37ae2e8bd4ee0d95313002d8b835acc20b
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-03 15:39:50 -0400

    added familiesReducer

---
+++ src/reducers/familiesReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  families: [],
+  loading: true
+}
+
+const familiesReducer = (state, action) => {
+  switch(action.type){
+    default:
+      return state;
+  }
+}
+ 
+export default familiesReducer;
