0a8dfcac998f2e2a3ba109a6638b7d5567e48fef
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-03 16:02:27 -0400

    imported thunk to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
 import familiesReducer from './reducers/familiesReducer'
