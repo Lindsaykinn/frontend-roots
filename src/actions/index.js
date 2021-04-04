8e647df58c4ca9ab4103c3812c877f30285e9b9c
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-03 21:04:25 -0400

    addFamily function to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,22 @@ export const getFamilies = () => {
     .then(families => dispatch({ type: 'SET_FAMILIES', families}))
   }
 }
+
+export const addFamily = (family, history) => {
+  return dispatch => {
+    fetch('http://localhost:3001/families', {
+      method: 'POST', 
+      headers: {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({ family })
+    })
+    .then(resp => resp.json())
+    .then(family => {
+      dispatch({ type: 'ADD_FAMILY', family})
+      history.push('/families')
+    })
+
+  }
+}
