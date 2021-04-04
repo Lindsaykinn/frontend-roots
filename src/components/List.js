115a369bf0453072073bc138649aa03c802cc2d6
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-03 21:05:50 -0400

    added Family List render to List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,23 @@
-const List = () => {
-  return ( 
-    <div>
-      <h2>Family List</h2>
-    </div>
-   );
-}
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Family from './Family'
+
+class List extends Component {
+  render(){
+    const families = this.props.families.map( (family, i) => <Family key={i} name={family.name} />)
+    return ( 
+      <div>
+        <h2>All Families</h2>
+        { families }
+      </div>
+     );
+  }
+  }
+
+  const mapStateToProps = state => {
+    return {
+      families: state.families
+    }
+  }
  
-export default List;
+export default connect(mapStateToProps)(List);
