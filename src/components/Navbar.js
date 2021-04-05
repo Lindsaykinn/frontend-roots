60f60965762c9dc00760a7154c59d82d2824492a
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-05 13:13:30 -0400

    updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,9 @@ const Navbar = () => {
     <nav className="navbar">
       <Link to='/'><h1>Roots</h1></Link>
       <div className="links">
-      <Link to='/families/new' style={{
-        color: '#e4eddb',
-        backgroundColor: '#144d53',
-        borderRadius: '8px'
-      }}>Add Family</Link>
+      <Link to='/families/new'>Add Family</Link>
       &nbsp;
-      <Link to='/families' style={{
-        color: '#e4eddb',
-        backgroundColor: '#144d53',
-        borderRadius: '8px'
-      }}>All Families</Link>
+      <Link to='/families'>All Families</Link>
       </div>
     </nav>
    );
