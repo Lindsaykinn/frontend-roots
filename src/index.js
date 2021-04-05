7f9d2d85487f7cacffd6e63457f24841a8875edb
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-05 13:14:05 -0400

    css import to index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-// import './index.css';
+import './index.css';
 import App from './App';
 import familiesReducer from './reducers/familiesReducer'
 
