2115ad5ec9f552ccae05b660eb678951c2fd3e25
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-05 13:13:15 -0400

    working on familydetails page

--- src/components/FamilyDetails.js
+++ src/components/FamilyDetails.js
@@ -1,7 +1,18 @@
+import {useParams} from 'react-router-dom'
+import {getFamilies} from './actions'
+
 const FamilyDetails = () => {
+  const { id } = useParams()
+  const { family } = getFamilies('http://localhost:3001/families' + id)
+  
   return ( 
     <div>
-      <h2>Family Details</h2>
+     {family && (
+       <article>
+         <h2>{family.name}</h2>
+         {/* <p>Stories: {family.story}</p> */}
+       </article>
+     )}
     </div>
    );
 }
