1d6a5fc8851afee93dfe0106e98356b9ceda1888
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-07 12:30:18 -0400

    updated FamilyDetails with useFetch

--- src/components/FamilyDetails.js
+++ src/components/FamilyDetails.js
@@ -1,18 +1,32 @@
-import {useParams} from 'react-router-dom'
-import {getFamilies} from './actions'
+import { useHistory, useParams } from "react-router"
+import useFetch from "../actions/useFetch"
 
 const FamilyDetails = () => {
   const { id } = useParams()
-  const { family } = getFamilies('http://localhost:3001/families' + id)
-  
+  const { data: family, error, isPending} = useFetch('http://localhost:3001/families/' + id)
+  const history = useHistory()
+
+  const handleClick = () => {
+    fetch('http://localhost:3001/families/' + family.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/families')
+    })
+  }
+
   return ( 
     <div>
-     {family && (
-       <article>
-         <h2>{family.name}</h2>
-         {/* <p>Stories: {family.story}</p> */}
-       </article>
-     )}
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { family && (
+        <article>
+          <h2>{ family.surname }</h2>
+          <p>{ family.image }</p>
+          <p>{ family.story } </p>
+      
+          <button onClick={handleClick}>Delete Family</button>
+        </article>
+      )}
     </div>
    );
 }
