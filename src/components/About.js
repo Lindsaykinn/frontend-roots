5fed5bcb96bd46a55281f7bbe6302cb44ca5ef7c
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-11 22:13:21 -0400

    added About component

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+const About = () => {
+  return ( 
+    
+    <p>Welcome to Roots!  An application where you can share information about your family history.  This page was created by Lindsay Kinn.  For any questions, comments, or suggestions, please reach out to me at lindsay.kinn@gmail.com. </p>
+   );
+}
+ 
+export default About;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return ( 
       <div className='footer'>
         &copy; Property of Lindsay Kinn
+        <Link to="/about"><h5>About this Website</h5></Link>
       </div>
    );
 }
