c58b411f09f17591d6c40d1e3c374e60c0c52520
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-17 15:25:48 -0400

    added alt desc to images

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -22,67 +22,67 @@ const Slideshow = () => {
       <Fade>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[0]} />
+            <img src={fadeImages[0]} alt="Graham Women" />
           </div>
           <h2>The Graham Women</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[1]} />
+            <img src={fadeImages[1]} alt="Baby Ruthe"/>
           </div>
           <h2>Ruth Eleanor Marshall - Age 1</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[2]} />
+            <img src={fadeImages[2]} alt="CV Marshalls" />
           </div>
           <h2>The CV Marshalls</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[3]} />
+            <img src={fadeImages[3]} alt="CV Marshalls" />
           </div>
           <h2>The CV Marshalls</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[4]} />
+            <img src={fadeImages[4]} alt="Dean Marshalls"/>
           </div>
           <h2>The Dean Marshalls</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[5]} />
+            <img src={fadeImages[5]} alt="Hare Girls" />
           </div>
           <h2>Hare Girls - 1965</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[6]} />
+            <img src={fadeImages[6]} alt="Larry Kinn" />
           </div>
           <h2>Larry Kinn</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[7]} />
+            <img src={fadeImages[7]} alt="Marshall Cousins" />
           </div>
           <h2>Leland, Ruthe, Phyllis, Phil Marshall</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[8]} />
+            <img src={fadeImages[8]} alt="Cousins" />
           </div>
           <h2>Phyllis & Ruthe Marshall</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[9]} />
+            <img src={fadeImages[9]} alt="Cousins" />
           </div>
           <h2>Ruthe & Phyllis Marshall</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <img src={fadeImages[10]} />
+            <img src={fadeImages[10]} alt="Ruthe"/>
           </div>
           <h2>Ruthe Marshall</h2>
         </div>
