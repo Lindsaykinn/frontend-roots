922cc2f1bd863338a2968952e4666fd768f6f284
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-19 14:00:10 -0400

    implemented useSelector hook

--- src/components/FamilyList.js
+++ src/components/FamilyList.js
@@ -1,25 +1,40 @@
-import { connect } from 'react-redux'
+import { useEffect } from 'react'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import {Link, Route} from 'react-router-dom'
 import FamilyDetails from './FamilyDetails'
+import {getFamilies, getFamily} from '../_actions/index'
 
-const FamilyList =({match, families}) =>{
+const FamilyList =() =>{
+  const families = useSelector(state => state.families.families)
+  const isLoading = useSelector(state => state.families.loading)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getFamilies())
+  }, []) 
+
+  if (isLoading){
+    return(<div>
+      Loading...
+    </div>)
+  }
+
   return ( 
     <div>
       <h1>All Families</h1>
       {families.map((family, idx) => (
        <Link key={idx} to={`/families/${family.id}`}>
         <h2>{family.surname}</h2>
-        <Route path={`${match.url}/:familyId`} component={FamilyDetails}/>
         </Link>
       ))}
     </div>
    );
 }
 
-  const mapStateToProps = state => {
-    return {
-      families: state.families.families
-    }
-  }
+  // const mapStateToProps = state => {
+  //   return {
+  //     families: state.families.families
+  //   }
+  // }
  
-export default connect(mapStateToProps)(FamilyList);
+export default FamilyList;
