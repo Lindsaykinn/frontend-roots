d8203c58c596d2087206de76b8060bc1440aba3f
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-19 13:59:35 -0400

    implemented useSelector hook

--- src/components/FamilyDetails.js
+++ src/components/FamilyDetails.js
@@ -1,34 +1,44 @@
-import { connect } from "react-redux"
+import { useEffect } from "react"
+import { connect, useDispatch, useSelector } from "react-redux"
 import { useParams, useHistory } from "react-router"
-import { deleteFamily } from '../_actions'
-import useFetch from "../_actions/useFetch"
+import { deleteFamily, getFamily } from '../_actions'
 
 
 const FamilyDetails = (props) => {
   const { id } = useParams()
-  const { data: family, error, isPending} = useFetch('http://localhost:3001/families/' + id)
+  const family = useSelector(state => state.families.family)
   const history = useHistory()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getFamily(id))
+  }, [])
+
+  if (family){
+    return ( 
+      <div>  
+        <div className='details'>
+          <div>
+            <h2>{ family.surname }</h2>
+            <p>Country of Origin: <b>{ family.country }</b></p>
+            <br/>
+            <p>Family History: { family.story } </p>
+            <br/>
+            <p>Genealogist: <b>{ family.person }</b> &nbsp; Date of Birth: { family.dob } </p>
+            </div> 
+            <br/>
+            <br/>
+          </div>      
+            <div>
+              <button onClick={() => props.deleteFamily(family, history)}>Delete Family</button>
+            </div>
+      </div>
+     );
+     
+  }
+  return(<div>Loading...</div>)
   
-  return ( 
-    <div>
-      { isPending && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { family && (
-            <div className='details'>
-              <div>
-                <h2>{ family.surname }</h2>
-                <p>Country of Origin: <b>{ family.country }</b></p>
-                <br/>
-                <p>Family History: { family.story } </p>
-                <br/>
-                <p>Genealogist: <b>{ family.person }</b> &nbsp; Date of Birth: { family.dob } </p>
-                </div> 
-                <br/>
-                <button onClick={() => props.deleteFamily(family, history)}>Delete Family</button>
-             </div>      
-      )}
-    </div>
-   );
+
 }
 
 const mapDispatchToProps = (dispatch) => { 
