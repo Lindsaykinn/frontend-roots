680b18e79fe939508330de4729a34c58fec1affa
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-04-29 17:18:59 -0400

    added person/people fetchess

--- src/_actions/index.js
+++ src/_actions/index.js
@@ -6,6 +6,14 @@ export const getFamilies = () => {
     .then(families => dispatch({ type: 'SET_FAMILIES', families}))
   }
 }
+export const getPeople = () => {
+  return dispatch => {
+    dispatch({type: 'LOADING'})
+    fetch(`http://localhost:3001/people`)
+    .then(resp => resp.json())
+    .then(people => dispatch({ type: 'SET_PEOPLE', payload:people}))
+  }
+}
 
 export const getFamily = (id) => {
   return dispatch => {
@@ -14,8 +22,18 @@ export const getFamily = (id) => {
     .then(family => dispatch({ type: 'GET_FAMILY', payload:family}))
   }
 }
+export const getPerson = (id) => {
+  return dispatch => {
+    fetch(`http://localhost:3001/people/${id}`)
+    .then(resp => resp.json())
+    .then(person => dispatch({ type: 'GET_PERSON', payload:person}))
+  }
+}
 
+
+//a, c, b, d
 export const addFamily = (family, history) => {
+  console.log('c')
   return dispatch => {
     fetch('http://localhost:3001/families', {
       method: 'POST', 
@@ -27,10 +45,30 @@ export const addFamily = (family, history) => {
     })
     .then(resp => resp.json())
     .then(family => {
+      console.log('d')
       dispatch({ type: 'ADD_FAMILY', payload:family})
       history.push('/families')
     })
+  }
+}
 
+export const addPerson = (person, history) => {
+  console.log('c')
+  return dispatch => {
+    fetch('http://localhost:3001/people', {
+      method: 'POST', 
+      headers: {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({ person })
+    })
+    .then(resp => resp.json())
+    .then(person => {
+      console.log('d')
+      dispatch({ type: 'ADD_PERSON', payload:person})
+      history.push('/people')
+    })
   }
 }
 
@@ -60,6 +98,24 @@ export const deleteFamily = (family, history) => {
       .then (() => {
         dispatch({ type: 'DELETE_FAMILY_SUCCESS', family})
         history.push('/families')
+      })
+    }
+  }
+
+export const deletePerson = (person, history) => {
+  return dispatch => {
+    console.log('delete_person_start')
+      dispatch({ type: 'DELETE_PERSON_START'})
+      fetch('http://localhost:3001/people/' + person.id, {
+        method: 'DELETE', 
+        headers: {
+          "Accept": 'application/json',
+          "Content-Type": 'application/json'
+        }
+      })
+      .then (() => {
+        dispatch({ type: 'DELETE_PERSON_SUCCESS', person})
+        history.push('/people')
       })
     }
   }
